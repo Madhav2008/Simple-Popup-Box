e4e20714a9011ffb5d4685b0d54b6af77e63614b
arora <aroramadhav65@gmail.com>
2022-05-30 17:38:25 +0530

    Update

--- script.js
+++
@@ -1,6 +0,0 @@
-let toogleBtn = document.querySelector('.toogleBtn');
-let container = document.querySelector('.container');
-
-toogleBtn.onClick = function () {
-    container.classList.toogle('active');
-}
